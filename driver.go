9ff52e2261aab5d5aabad3ed9a3081280d543c84
Brian Sorahan <bsorahan@gmail.com>
2017-04-25 20:36:04 -0500

    SELECT InitSpatialMetaData(); when creating a new connection

--- driver.go
+++ driver.go
@@ -9,13 +9,44 @@ import "C"
 
 import (
 	"database/sql"
+	"database/sql/driver"
 
 	sqlite "github.com/mattn/go-sqlite3"
 )
 
+// Conn is a spatialite database connection.
+type Conn struct {
+	*sqlite.SQLiteConn
+}
+
 // Driver is the spatialite driver.Driver implementation.
 type Driver struct {
 	*sqlite.SQLiteDriver
+}
+
+// Open opens a new database connection.
+func (d *Driver) Open(name string) (driver.Conn, error) {
+	slconn, err := d.SQLiteDriver.Open(name)
+	if err != nil {
+		return nil, err
+	}
+	stmt, err := slconn.Prepare(`SELECT InitSpatialMetaData()`)
+	if err != nil {
+		return nil, err
+	}
+	defer func() { _ = stmt.Close() }()
+
+	rows, err := stmt.Query(nil)
+	if err != nil {
+		return nil, err
+	}
+	// We don't need the rows.
+	if err := rows.Close(); err != nil {
+		return nil, err
+	}
+	return &Conn{
+		SQLiteConn: slconn.(*sqlite.SQLiteConn),
+	}, nil
 }
 
 func init() {
